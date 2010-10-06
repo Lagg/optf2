4c122d4621f54b2bc2cc052870e31e92d833b87b
Anthony Garcia <lagg@lavabit.com>
2010-10-06 00:19:18 -0700

    Better dialogs/overlay, loading notification
    Image resizing stuff still needs to be done, but this is a start

--- static/optf2.js
+++ static/optf2.js
@@ -2,16 +2,40 @@ $(document).ready(function(){
     $(".item_link").removeAttr("href");
 });
 
-function item_overlay_success(elem) {
-    $("#navbar").remove();
-    $("#item_stats").hide().fadeIn('slow');
+function item_resize(event, ui) {
+    var item = $(event.target);
+
+    item.height(ui.size.height);
+    item.width(ui.size.width);
+    item.find("#stat_vertrule").height(ui.size.height);
 }
 
-function item_open(item_url) {
-    if (($(window).height() - 100) <= 600 ||
-        ($(window).width() - 100) <= 800) {
-        window.location = item_url;
-    } else {
-        $().jOverlay({url: item_url, onSuccess: item_overlay_success});
+function item_open_success(data, status, xhr) {
+    var page = $(data);
+    var dialog_title = page.filter("#header");
+    var dialog_content = page.filter("#content").find("#item_stats");
+    var dialog_width = 850;
+    var dialog_height = 700;
+
+    if ($(document).height() < dialog_height) {
+        dialog_height = $(document).height() - dialog_title.height();
     }
+    if ($(document).width() < dialog_width) {
+        dialog_width = $(document).width();
+    }
+
+    $("#loading_notice").remove();
+
+    $(dialog_content).dialog({
+        resize: item_resize,
+        title: dialog_title,
+        width: dialog_width,
+        height: dialog_height,
+        modal: true
+    });
+}
+
+function item_open(item_url, item_id) {
+    $("#" + item_id).prepend("<div id=\"loading_notice\"><b>Loading...</b></div>");
+    $.get(item_url, item_open_success, {}, "html");
 }
