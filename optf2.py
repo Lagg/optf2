c7cf20857bd44c0e540383bf94997a7dbda1c54e
Anthony Garcia <lagg@lavabit.com>
2010-12-18 02:09:59 -0700

    Phase 3: Split item functions

--- optf2.py
+++ optf2.py
@@ -23,10 +23,14 @@ try:
     import hmac, hashlib
     import steam, os, json, urllib2
     import web
-    from web import form
     from copy import deepcopy
     import cPickle as pickle
-    import config, database
+    import config
+    steam.set_api_key(config.api_key)
+    steam.set_language(config.language)
+    steam.set_cache_dir(config.cache_file_dir)
+    web.ctx.pack = steam.backpack()
+    import database, itemtools
 except ImportError as E:
     print(str(E))
     raise SystemExit
@@ -49,40 +53,13 @@ urls = (
 # if the user is a Valve employee)
 valve_group_id = 103582791429521412
 
-qualitydict = {"unique": "The",
-               "normal": ""}
-
-# I don't like this either but Valve didn't expose them
-# through the API
-particledict = {0: "Invalid Particle",
-                1: "Particle 1",
-                2: "Flying Bits",
-                3: "Nemesis Burst",
-                4: "Community Sparkle",
-                5: "Holy Glow",
-                6: "Green Confetti",
-                7: "Purple Confetti",
-                8: "Haunted Ghosts",
-                9: "Green Energy",
-                10: "Purple Energy",
-                11: "Circling TF Logo",
-                12: "Massed Flies",
-                13: "Burning Flames",
-                14: "Scorching Flames",
-                15: "Searing Plasma",
-                16: "Vivid Plasma",
-                17: "Sunbeams",
-                18: "Circling Peace Sign",
-                19: "Circling Heart",
-                20: "Map Stamps"}
-
 # These should stay explicit
 render_globals = {"css_url": config.css_url,
                   "virtual_root": config.virtual_root,
                   "static_prefix": config.static_prefix,
                   "encode_url": web.urlquote,
                   "len": len,
-                  "particledict": particledict,
+                  "particledict": itemtools.particledict,
                   "instance": web.ctx,
                   "project_name": config.project_name,
                   "project_homepage": config.project_homepage,
@@ -95,16 +72,11 @@ app = web.application(urls, globals())
 templates = web.template.render(config.template_dir, base = "base",
                                 globals = render_globals)
 
-steam.set_api_key(config.api_key)
-steam.set_language(config.language)
-steam.set_cache_dir(config.cache_file_dir)
-
 logging.basicConfig(filename = os.path.join(steam.get_cache_dir(), "optf2.log"), level = logging.DEBUG)
 
 db_obj = config.database_obj
 
-store = web.session.DBStore(db_obj, "sessions")
-session = web.session.Session(app, store)
+session = web.session.Session(app, web.session.DBStore(db_obj, "sessions"))
 
 openid_secret = os.path.join(steam.get_cache_dir(), "oid_super_secret")
 if not os.path.exists(openid_secret):
@@ -132,276 +104,7 @@ def openid_get_id():
     return None
 render_globals["get_openid"] = openid_get_id
 
-web.ctx.pack = steam.backpack()
 pack = web.ctx.pack
-
-def get_invalid_pos_items(items):
-    poslist = []
-    invalid_items = []
-    for item in items:
-        if not item: continue
-        pos = pack.get_item_position(item)
-        if pos != -1 and pos not in poslist:
-            poslist.append(pack.get_item_position(item))
-        else:
-            for item in items:
-                if item and item not in invalid_items and pos == pack.get_item_position(item):
-                    invalid_items.append(deepcopy(item))
-
-    return invalid_items
-
-def sort_items(items, sortby):
-    itemcmp = None
-    def defcmp(x, y):
-        if x < y:
-            return -1
-        elif x > y:
-            return 1
-        elif x == y:
-            return 0
-
-    if sortby == "time":
-        items.reverse()
-
-    if sortby == "serial":
-        def itemcmp(x, y):
-            return defcmp(pack.get_item_id(x),
-                          pack.get_item_id(y))
-    elif sortby == "cell":
-        def itemcmp(x, y):
-            return defcmp(pack.get_item_position(x),
-                          pack.get_item_position(y))
-    elif sortby == "level":
-        def itemcmp(x, y):
-            return defcmp(pack.get_item_level(x),
-                          pack.get_item_level(y))
-    elif sortby == "name":
-        def itemcmp(x, y):
-            return defcmp(pack.get_item_quality(x)["str"] + " " + pack.get_item_name(x),
-                          pack.get_item_quality(y)["str"] + " " + pack.get_item_name(y))
-    elif sortby == "slot":
-        def itemcmp(x, y):
-            return defcmp(pack.get_item_slot(x), pack.get_item_slot(y))
-    elif sortby == "class":
-        def itemcmp(x, y):
-            cx = pack.get_item_equipable_classes(x)
-            cy = pack.get_item_equipable_classes(y)
-            lenx = len(cx)
-            leny = len(cy)
-
-            if lenx == 1 and leny == 1:
-                return defcmp(cx[0], cy[0])
-            else:
-                return defcmp(lenx, leny)
-
-    if itemcmp:
-        items.sort(cmp = itemcmp)
-    if sortby == "cell":
-        newitems = [None] * config.backpack_padded_size
-        for item in items:
-            pos = pack.get_item_position(item) - 1
-            try:
-                if pos > config.backpack_padded_size:
-                    while pos > config.backpack_padded_size:
-                        newitems += ([None] * 100)
-                        config.backpack_padded_size += 100
-                if pos > -1 and newitems[pos] == None:
-                    newitems[pos] = deepcopy(item)
-            except IndexError: pass
-        return newitems
-    else:
-        if len(items) < config.backpack_padded_size:
-            items += ([None] * (config.backpack_padded_size - len(items)))
-    return items
-
-def filter_items_by_class(items, theclass):
-    filtered_items = []
-
-    for item in items:
-        if not item: continue
-        classes = pack.get_item_equipable_classes(item)
-        for c in classes:
-            if c == theclass:
-                filtered_items.append(item)
-                break
-    return filtered_items
-
-def get_item_stats(items):
-    """ Returns a dict of various backpack stats """
-    stats = {"weapons": 0,
-             "misc": 0,
-             "hats": 0,
-             "total": 0}
-
-    for item in items:
-        if not item: continue
-
-        slot = pack.get_item_slot(item)
-        iclass = pack.get_item_class(item)
-
-        stats["total"] += 1
-
-        if slot == "primary" or slot == "melee" or slot == "secondary":
-            if iclass.find("token") == -1:
-                stats["weapons"] += 1
-        elif slot == "head" and iclass.find("token") == -1:
-            stats["hats"] += 1
-        elif slot == "misc":
-            stats["misc"] += 1
-    return stats
-
-def absolute_url(relative_url):
-    domain = web.ctx.homedomain
-    if domain.endswith('/'): domain = domain[:-1]
-    return domain + relative_url
-
-def process_attributes(items):
-    """ Filters attributes for the item list,
-    optf2-specific keys are prefixed with optf2_ """
-
-    for item in items:
-        if not item: continue
-        attrs = pack.get_item_attributes(item)
-        item["optf2_untradeable"] = pack.is_item_untradeable(item)
-        item["optf2_attrs"] = []
-        item["optf2_description"] = pack.get_item_description(item)
-        item["optf2_image_url"] = pack.get_item_image(item, pack.ITEM_IMAGE_SMALL)
-        item["optf2_image_url_large"] = pack.get_item_image(item, pack.ITEM_IMAGE_LARGE)
-        min_level = pack.get_item_min_level(item)
-        max_level = pack.get_item_max_level(item)
-        custom_desc = pack.get_item_custom_description(item)
-
-        if custom_desc: item["optf2_description"] = custom_desc
-
-        if min_level == max_level:
-            item["optf2_level"] = str(min_level)
-        else:
-            item["optf2_level"] = str(min_level) + "-" + str(max_level)
-
-        for attr in attrs:
-            desc = pack.get_attribute_description(attr)
-
-            if pack.get_attribute_name(attr) == "cannot trade":
-                item["optf2_untradeable"] = True
-                continue
-
-            # Workaround until Valve gives sane values
-            if (pack.get_attribute_value_type(attr) != "date" and
-                attr["value"] > 1000000000 and
-                "float_value" in attr):
-                attr["value"] = attr["float_value"]
-
-            # Contained item is a schema id
-            if pack.get_attribute_name(attr) == "referenced item def":
-                sival = int(pack.get_attribute_value(attr))
-                sitem = pack.get_item_by_schema_id(sival)
-                item["optf2_gift_content"] = "an invalid item"
-
-                if item:
-                    item["optf2_gift_content"] = pack.get_item_name(sitem)
-                    item["optf2_gift_content_id"] = sival
-                attr["description_string"] = 'Contains ' + item["optf2_gift_content"]
-                attr["hidden"] = False
-
-            if pack.get_attribute_name(attr) == "set item tint RGB":
-                raw_rgb = int(pack.get_attribute_value(attr))
-                # Set to purple for team colored paint
-                if pack.get_item_schema_id(item) != 5023 and raw_rgb == 1:
-                    item_color = 'url("{0}team_splotch.png")'.format(config.static_prefix)
-                else:
-                    item_color = "#{0:02X}{1:02X}{2:02X}".format((raw_rgb >> 16) & 0xFF,
-                                                                 (raw_rgb >> 8) & 0xFF,
-                                                                 (raw_rgb) & 0xFF)
-
-                # Workaround until the icons for colored paint cans are correct
-                schema_paintcan = pack.get_item_by_schema_id(pack.get_item_schema_id(item))
-                if (schema_paintcan and
-                    schema_paintcan.get("name", "").startswith("Paint Can") and
-                    raw_rgb != 1 and raw_rgb != 0):
-                    paintcan_url = "{0}item_icons/Paint_Can_{1}.png".format(config.static_prefix,
-                                                                            item_color[1:])
-                    item["optf2_image_url"] = absolute_url(paintcan_url)
-                    item["optf2_image_url_large"] = absolute_url(paintcan_url)
-                item["optf2_color"] = item_color
-                continue
-
-            if pack.get_attribute_name(attr) == "attach particle effect":
-                attr["description_string"] = ("Effect: " +
-                                              particledict.get(int(attr["value"]), particledict[0]))
-
-            if pack.get_attribute_name(attr) == "gifter account id":
-                attr["description_string"] = "Gift"
-                item["optf2_gift_from"] = "7656" + str(int(pack.get_attribute_value(attr) +
-                                                           1197960265728))
-                try:
-                    user = database.load_profile_cached(item["optf2_gift_from"], stale = True)
-                    item["optf2_gift_from_persona"] = user.get_persona()
-                    attr["description_string"] = "Gift from " + item["optf2_gift_from_persona"]
-                except:
-                    item["optf2_gift_from_persona"] = "this user"
-
-            if "description_string" in attr and not pack.is_attribute_hidden(attr):
-                attr["description_string"] = web.websafe(attr["description_string"])
-            else:
-                continue
-            item["optf2_attrs"].append(deepcopy(attr))
-
-        quality_str = pack.get_item_quality(item)["str"]
-        pretty_quality_str = pack.get_item_quality(item)["prettystr"]
-        prefix = qualitydict.get(quality_str, pretty_quality_str)
-        custom_name = pack.get_item_custom_name(item)
-        item_name = pack.get_item_name(item)
-
-        if item_name.find("The ") != -1 and pack.is_item_prefixed(item):
-            item_name = item_name[4:]
-
-        if custom_name or (not pack.is_item_prefixed(item) and quality_str == "unique"):
-            prefix = ""
-        if custom_name:
-            item_name = custom_name
-
-        item["optf2_cell_name"] = '<div class="{0}_name item_name">{1} {2}</div>'.format(
-            quality_str, prefix, item_name)
-
-        if custom_name or (not pack.is_item_prefixed(item) and quality_str == "unique"):
-            prefix = ""
-        else:
-            prefix = pretty_quality_str
-        color = item.get("optf2_color")
-        paint_job = ""
-        if color:
-            if color.startswith("url"):
-                color = "#FF00FF"
-            paint_job = '<span style="color: {0}; font-weight: bold;">Painted</span>'.format(color)
-        item["optf2_dedicated_name"] = "{0} {1} {2}".format(paint_job, prefix, item_name)
-
-        if color:
-            paint_job = "Painted"
-        item["optf2_title_name"] = "{0} {1} {2}".format(paint_job, prefix, item_name)
-
-        if color:
-            paint_job = "(Painted)"
-        else:
-            paint_job = ""
-        if prefix:
-            prefix = qualitydict.get(quality_str, pretty_quality_str)
-
-        item["optf2_feed_name"] = "{0} {1} {2}".format(prefix, item_name, paint_job)
-
-    return items
-
-def get_equippable_classes(items):
-    """ Returns a set of classes that can equip this
-    item """
-
-    valid_classes = set()
-
-    for item in items:
-        if not item: continue
-        classes = pack.get_item_equipable_classes(item)
-        if classes[0]: valid_classes |= set(classes)
-
-    return valid_classes
 
 def internalerror():
     logging.error(traceback.format_exc())
@@ -417,11 +120,11 @@ class schema_dump:
             items = pack.get_items(from_schema = True)
 
             if "sortclass" in query:
-                items = filter_items_by_class(items, query["sortclass"])
+                items = itemtools.filter_by_class(items, query["sortclass"])
 
-            filter_classes = get_equippable_classes(items)
+            filter_classes = itemtools.get_equippable_classes(items)
 
-            return templates.schema_dump(pack, process_attributes(items), filter_classes)
+            return templates.schema_dump(pack, itemtools.process_attributes(items), filter_classes)
         except:
             return templates.error("Couldn't load schema")
 
@@ -447,7 +150,7 @@ class attrib_dump:
                             attached_items.append(item)
                             break
 
-                return templates.schema_dump(pack, process_attributes(attached_items), [], attachment_check)
+                return templates.schema_dump(pack, itemtools.process_attributes(attached_items), [], attachment_check)
 
             if query.get("wikitext"):
                 web.header("Content-Type", "text/plain; charset=UTF-8")
@@ -512,7 +215,7 @@ class pack_item:
                     if backpack and pack.get_item_id(theitem) not in pickle.loads(str(backpack["backpack"])):
                         item_outdated = True
 
-            item = process_attributes([theitem])[0]
+            item = itemtools.process_attributes([theitem])[0]
         except Exception:
             return templates.item_error_notfound(idl[1])
         return templates.item(user, item, pack, item_outdated)
@@ -578,16 +281,16 @@ class pack_fetch:
         try:
             items = database.load_pack_cached(user)
 
-            filter_classes = get_equippable_classes(items)
+            filter_classes = itemtools.get_equippable_classes(items)
             if sortclass:
-                items = filter_items_by_class(items, sortclass)
+                items = itemtools.filter_by_class(items, sortclass)
 
-            process_attributes(items)
-            stats = get_item_stats(items)
+            itemtools.process_attributes(items)
+            stats = itemtools.get_stats(items)
 
-            baditems = get_invalid_pos_items(items)
+            baditems = itemtools.get_invalid_pos(items)
 
-            items = sort_items(items, sortby)
+            items = itemtools.sort(items, sortby)
 
             for bitem in baditems:
                 if bitem in items:
@@ -641,7 +344,7 @@ class pack_feed:
         try:
             user = database.load_profile_cached(sid, stale = True)
             items = database.load_pack_cached(user)
-            process_attributes(items)
+            itemtools.process_attributes(items)
         except Exception as E:
             return templates.error(str(E))
         web.header("Content-Type", "application/rss+xml")
